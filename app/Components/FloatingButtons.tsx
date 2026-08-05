8800e7661c547f1d500b5ced8446ef9d2d60eb6f
ko1218 <kingjr1980@gmail.com>
2026-08-05 17:18:32 +0800

    更新浮動快捷按鈕圖示

--- app/Components/FloatingButtons.tsx
+++ app/Components/FloatingButtons.tsx
@@ -1,20 +1,27 @@
+"use client";
+
+import {
+  FaPhoneAlt,
+  FaFacebookF,
+  FaMapMarkerAlt,
+  FaArrowUp,
+} from "react-icons/fa";
+
 export default function FloatingButtons() {
   const btn =
-    "flex h-14 w-14 items-center justify-center rounded-full bg-[#8d7459] text-2xl text-white shadow-lg transition hover:scale-110";
+    "flex h-14 w-14 items-center justify-center rounded-full bg-[#8d7459] text-white shadow-lg transition duration-300 hover:scale-110 hover:bg-[#705b46]";
 
   return (
     <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3">
 
-      {/* 一鍵來電 */}
       <a
         href="tel:0926753345"
         className={btn}
         title="立即來電"
       >
-        📞
+        <FaPhoneAlt size={20} />
       </a>
 
-      {/* Facebook */}
       <a
         href="https://www.facebook.com/profile.php?id=61592791742605"
         target="_blank"
@@ -22,10 +29,9 @@ export default function FloatingButtons() {
         className={btn}
         title="Facebook"
       >
-        👍
+        <FaFacebookF size={20} />
       </a>
 
-      {/* Google 地圖 */}
       <a
         href="https://maps.google.com/?q=騰駿水電工程有限公司"
         target="_blank"
@@ -33,11 +39,11 @@ export default function FloatingButtons() {
         className={btn}
         title="Google 地圖"
       >
-        📍
+        <FaMapMarkerAlt size={20} />
       </a>
 
-      {/* 回到頂端 */}
       <button
+        type="button"
         onClick={() =>
           window.scrollTo({
             top: 0,
@@ -47,7 +53,7 @@ export default function FloatingButtons() {
         className={btn}
         title="回到頂端"
       >
-        ⬆️
+        <FaArrowUp size={20} />
       </button>
 
     </div>
